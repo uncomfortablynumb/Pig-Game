311d8fa729586ff4253cccba31e1bdf103b3b085
Wang Xuzhe <wangxzh36@mail2.sysu.edu.cn>
2021-08-26 09:37:26 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,163 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying, lastDice;
+
+init();
+
+// Set player 1/2's current dice point
+// - this could only change text content, not HTML, so it can't output italic numbers
+// Here if we want to change HMTL:
+// - document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// function btn() {
+//   // do sth here
+// }
+// btn();
+
+// call-back function
+// document.querySelector('.btn-roll').addEventListener('click',btn);
+
+document.querySelector('.btn-roll').addEventListener('click',function() {
+  if (gamePlaying) {
+    // 1. Random number
+    // var dice = Math.floor(Math.random() * 6) + 1;
+
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    
+    // querySelector only selects the first element;
+    // need to use for loop to implement on several elements with the same class
+
+    // var diceDOM = document.querySelector('.dice');
+    // diceDOM.style.display = 'block';
+
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice1 !== 1 && dice2 !== 1) {
+      // Add score
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+    // if (lastDice === dice && dice === 6) {
+    //   scores[activePlayer] = 0;
+    //   // display clearing to 0
+    //   document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //   nextPlayer();
+    // } else {
+    //   lastDice = dice;
+    // }
+  }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  if (gamePlaying) {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    var input = document.querySelector('.final-score').value;
+    var winningScore;
+    
+    // Undefined, 0, null or "" are COERCED to false
+    // Anything else is COERCED to true
+    if (input) {
+      winningScore = input;
+    } else {
+      winningScore = 100;
+    }
+
+    // Check if player won the game
+    if (scores[activePlayer] >= winningScore) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      // Next player
+      nextPlayer();
+    }
+  }
+
+});
+
+function nextPlayer() {
+  // Next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  // set state variable
+  gamePlaying = true;
+
+  // why only the orginal controled?
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+/**
+ * 1. rolls two 6 points -> lose entire score
+ * (save precious dice roll)
+ *  
+ * 2.add an input field in HTML to set winning score (JS: .value to read the value)
+ * 
+ * 3. add another dice, loose current score with 1 dice of point 1(, but can continue with the other one)
+ */
+
+
